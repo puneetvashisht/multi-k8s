b56d5b24a4513e7be400cfef04da797781b8fd51
puneetvashisht@gmail.com <puneetvashsiht@gmail.com>
2019-12-12 14:07:37 +0530

    changes

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t puneetvashisht/multi-client:latest -t puneetvashisht/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t puneetvashisht/multi-server:latest -t puneetvashisht/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t puneetvashisht/multi-worker:latest -t puneetvashisht/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
-docker push stephengrider/multi-client:latest
-docker push stephengrider/multi-server:latest
-docker push stephengrider/multi-worker:latest
+docker push puneetvashisht/multi-client:latest
+docker push puneetvashisht/multi-server:latest
+docker push puneetvashisht/multi-worker:latest
 
-docker push stephengrider/multi-client:$SHA
-docker push stephengrider/multi-server:$SHA
-docker push stephengrider/multi-worker:$SHA
+docker push puneetvashisht/multi-client:$SHA
+docker push puneetvashisht/multi-server:$SHA
+docker push puneetvashisht/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=puneetvashisht/multi-server:$SHA
+kubectl set image deployments/client-deployment client=puneetvashisht/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=puneetvashisht/multi-worker:$SHA
